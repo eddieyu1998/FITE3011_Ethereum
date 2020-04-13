36f64245eec9dcfa535b26b5f56d5725fa378bb0
eddieyu <eddieyu1998@gmail.com>
2020-04-14 00:44:18 +0800

    added some idea on draw function

--- contracts/Crowdfunding_127.sol
+++ contracts/Crowdfunding_127.sol
@@ -2,15 +2,25 @@ pragma solidity ^0.5.16;
 
 /** Design
 *   1. need to store a list of crowdfunding campaign
-*   2. Each contains a owner (address), endtime (uint), goal (uint), deposit (uint), number of donors (uint), each donor (address) and his amount (uint)
+*   2. Each contains a owner (address), endtime (uint), goal (uint), deposit (uint), number of donors (uint), each donor (address) and his amount (uint), +status (bool) (drawed)
+*
 *   methods:
 *   1. Get the current total deposit
 *   2. Get the endtime
 *   3. Get the goal
-*   4. Create campaign
-*   5. Allow donor to back (address, amount) -> check amount valid, goal haven't achieved, endtime not reached (allow re-donation??)
+*   4. Create campaign (can a person create more than one?)
+*   5. Allow donor to donate (address, amount) -> check amount valid, goal haven't achieved, endtime not reached (allow re-donation??) (allow owner to donate?)
 *   6. Allow owner withdraw if goal achieved -> transfer
 *   7. Allow donor withdraw if goal not achieved, when endtime reached
+*
+*   Problems:
+*   1. Owner may double draw: campaign state (active, drawn, ended)
+*   2. endTime issue, last minute draw, time back donate: include a settledown period (+900s), after that can action be perform
+*
+*
+*   Security:
+*   1. variable private modifier needed?
+*
 * */
 
 contract Crowdfunding
@@ -75,7 +85,7 @@ contract Crowdfunding
     {
         // validate campaignId
         require (campaignId < numCampaigns, "Invalid campaignId");
-        
+
         Campaign storage c = campaigns[campaignId];
 
         // require endTime not reached
@@ -86,10 +96,36 @@ contract Crowdfunding
 
         // require transferring amount > 0
         require (msg.value > 0, "Invalid donation amount");
-        
+
         c.donors[c.numDonors++] = Donor(msg.sender, msg.value);
         c.deposit += msg.value;
     }
 
     // Function to withdraw from a campaign
+    function withdraw (uint campaignId) public
+    {
+        // validate campaignId
+        require (campaignId < numCampaigns, "Invalid campaignId");
+
+        Campaign storage c = campaigns[campaignId];
+        if (msg.sender == c.owner)
+        {
+            // check if goal is reached
+            require (c.deposit >= c.goal, "Goal not reached");
+
+            // check if fund has already been drawn (campaign state)
+
+            //
+            msg.sender.transfer(c.deposit);
+
+            // prevent owner double draw
+            // change state
+        }
+        else
+        {
+            // check if sender took part in it
+
+            // allow refund, then remove from mapping
+        }
+    }
 }
